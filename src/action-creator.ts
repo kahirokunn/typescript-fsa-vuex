836240e56fc24634836278506dbc63f2f9176b37
k-okina <okinakahiro@gmail.com>
2019-01-28 16:27:29 +0900

    add: added a some safety features for debug

--- src/action-creator.ts
+++ src/action-creator.ts
@@ -60,9 +60,39 @@ export function actionCreator<Payload = void>(type: FluxType): ActionCreator<Pay
   ) as ActionCreator<Payload>;
 }
 
+const debugSettings = {
+  doNotUseSamePrefix: false,
+  doNotCreateSameFluxType: false,
+  logPrefix: false,
+  logFluxType: false,
+}
+const cachedPrefixList: string[] = []
+const cachedFluxTypeList: string[] = []
+export function debugSetting(settings: typeof debugSettings) {
+  Object.assign(debugSettings, settings)
+}
+
 export function actionCreatorFactory(prefix?: string) {
+  if (prefix && debugSettings.doNotUseSamePrefix) {
+    if (cachedPrefixList.includes(prefix)) {
+      throw Error(`Prefix [${prefix}] is already used.`)
+    }
+    cachedPrefixList.push(prefix)
+  }
+  if (debugSettings.logPrefix) {
+    console.log(`Success to used prefix! That is [${prefix}].`)
+  }
   return <Payload = void>(type: FluxType): ActionCreator<Payload> => {
     const base = prefix ? `${prefix}/${type}` : type
+    if (debugSettings.doNotCreateSameFluxType) {
+      if (cachedFluxTypeList.includes(base)) {
+        throw Error(`FluxType [${base}] is already used.`)
+      }
+      cachedFluxTypeList.push(base)
+    }
+    if (debugSettings.logFluxType) {
+      console.log(`Success to used FluxType! That is [${base}].`)
+    }
     return Object.assign(
       (payload: Payload, options: Options): FSA<Payload> => {
         return {
