52ffc8ad7650b8bd1ffae7b585744fc8b4a31fc5
k-okina <okinakahiro@gmail.com>
2019-01-30 14:14:16 +0900

    add: release actionToMutation function.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { combineAction, action, combineMutation, mutation } from './helpers'
+export { combineAction, action, actionToMutation, combineMutation, mutation } from './helpers'
 export { actionCreator } from './action-creator'
 export { actionCreatorFactory, debugSetting } from './action-creator-factory'
